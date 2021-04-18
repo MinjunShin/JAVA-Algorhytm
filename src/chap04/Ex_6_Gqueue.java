bb26d5695801afaed8d6492360072b698c0c5c03
MinjunShin <pj0014@naver.com>
2021-04-19 01:40:13 +0900

    Ex_6 Generic Queue 클래스 완성

--- src/chap04/Ex_6_Gqueue.java
+++ src/chap04/Ex_6_Gqueue.java
@@ -21,14 +21,52 @@ public class Ex_6_Gqueue <E> {
         front = rear = num = 0;
         max = capacity;
         try {
-            this.que = (E[]) que[capacity];
+            this.que = (E[]) que[max];
         } catch (OutOfMemoryError e) {
-        	max = 0;
+            max = 0;
         }
-
     }
 
+    public E enque(E x) throws OverflowQueueException {
+        if(num >= max)
+		    throw new OverflowQueueException();
+        que[rear++] = x;
+        num++;
+        if(rear == max)
+            rear = 0;
+        return x;
+    }
 
+    public E deque() throws EmptyQueueException {
+        if(num == 0)
+            throw new EmptyQueueException();
+        E x = que[front++];        
+        num--;
 
+        if(front == max)
+            front = 0;
+        return x;
+    }
+
+    public E peek() throws EmptyQueueException {
+        if(num <= 0)
+            throw new EmptyQueueException();
+        return que[front];
+    }
+
+    public int indexOf(E x) {
+        for(int i = 0; i < num ; i++) {
+            if(que[(i + front) % max] == x)
+                return i + front;
+        }
+        return -1;
+    }
+
+    public int search(E x) {
+        for (int i = 0; i < num; i++)
+            if (que[(i + front) % max].equals(x))   // 검색 성공
+                return i + 1;
+        return -1;                            	 // 검색 실패
+    }
 
 }
