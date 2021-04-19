fad6fc94c0ebae0aabb9a4be6e486b813317642f
MinjunShin <pj0014@naver.com>
2021-04-20 00:25:02 +0900

    5장 ~유클리드 호제법

---
+++ src/chap05/EuclidGCD.java
@@ -0,0 +1,25 @@
+package chap05;
+
+import java.util.Scanner;
+
+public class EuclidGCD {
+ 
+    static int gcd(int x, int y){
+        if(y == 0)
+            return x;
+
+        else
+            return gcd(y, x % y);
+    }
+
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        System.out.println("두 수의 최대 공약수를 구한다.");
+
+        System.out.print("첫번째 정수 입력 : "); int x = stdIn.nextInt();
+        System.out.print("두번째 정수 입력 : "); int y = stdIn.nextInt();
+
+        System.out.println("두 수의 최대 공약수는 : " + gcd(x,y));
+    }
+}
