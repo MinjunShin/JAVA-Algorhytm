7091d92ef6b51401c45ad6f9da31e88764ea0790
MinjunShin <pj0014@naver.com>
2021-05-02 18:01:30 +0900

    Ex_6 선택정렬 상세 출력

---
+++ src/chap06/Ex_6_Detail_SelectionSort.java
@@ -0,0 +1,61 @@
+package chap06;
+
+import java.util.Scanner;
+
+public class Ex_6_Detail_SelectionSort {
+
+    static void swap(int[] a, int idx1, int idx2) {
+        int tmp = a[idx1];
+        a[idx1] = a[idx2];
+        a[idx2] = tmp;
+    }
+
+    static void detail_SelecSort(int[] a, int n) {
+        for (int i = 0; i < n - 1; i++) {
+            // 아직 정렬되지 않은 부분의 가장 작은 요소의 인덱스를 기록한다.
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+
+                /*
+                for (int m = 0; m < n; m++)
+                    System.out.printf("%3s\n", (m == i) ? '*' : (a[m] < a[min]) ? '+' : ' ');
+                */
+                if (a[j] < a[min]) {
+                    /*
+                    for (int k = 0; k < n; k++)
+                        System.out.printf("%3d", a[k]);
+                    System.out.println();
+                    */
+                    min = j;
+                }
+            // 아직 정렬되지 않은 부분의 첫 요소와 가장 작은 요소를 교환한다.
+            swap(a, i, min);
+            }
+        }
+    }
+
+    static void showResult(int[] x, int n) {
+        System.out.println("선택 정렬 결과 출력 : ");
+        for (int i = 0; i < n; i++) {
+            System.out.println("x[" + i + "] : " + x[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        System.out.print("요소 값을 입력하세요.");
+        int n = stdIn.nextInt();
+        int[] x = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("x[" + i + "] : ");
+            int idx = stdIn.nextInt();
+            x[i] = idx;
+        }
+
+        System.out.println("선택 정렬 과정 출력 : ");
+        detail_SelecSort(x, n);
+
+        showResult(x, n);
+    }
+}
--- src/chap06/InsertionSort.java
+++ src/chap06/InsertionSort.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class InsertionSort {
 
-    static void insertSort(int[]a, int n) {
+    static void insertSort(int[] a, int n) {
 
-        for(int i = 0 ; i < n ; i ++) {
+        for (int i = 0; i < n; i++) {
             int j;
             int tmp = a[i];
-            for(j = i ; j > 0 && a[j-1] > tmp ; j--)
-                a[j] = a[j-1];
+            for (j = i; j > 0 && a[j - 1] > tmp; j--)
+                a[j] = a[j - 1];
             a[j] = tmp;
         }
     }
@@ -19,10 +19,10 @@ public class InsertionSort {
         Scanner stdIn = new Scanner(System.in);
         System.out.print("배열 요소 갯수를 입력하세요. :");
         int nx = stdIn.nextInt();
-        int[] x = new int [nx];
+        int[] x = new int[nx];
 
         System.out.println("배열 요소를 입력하세요.");
-        for(int i = 0; i < nx ; i++) {
+        for (int i = 0; i < nx; i++) {
             System.out.print("a[" + i + "] : ");
             x[i] = stdIn.nextInt();
         }
@@ -31,8 +31,8 @@ public class InsertionSort {
 
         System.out.println("단순 삽입 정렬 결과 : ");
 
-        for(int i = 0 ; i < nx ; i++)
-            System.out.println("x["+ i +"] : " + x[i]); 
+        for (int i = 0; i < nx; i++)
+            System.out.println("x[" + i + "] : " + x[i]);
     }
-    
+
 }
