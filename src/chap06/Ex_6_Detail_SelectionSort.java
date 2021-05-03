cd719394479efe93734434e5ca874ee47803aa88
MinjunShin <pj0014@naver.com>
2021-05-04 01:36:28 +0900

    중간 문법 수정

--- src/chap06/Ex_6_Detail_SelectionSort.java
+++ src/chap06/Ex_6_Detail_SelectionSort.java
@@ -14,24 +14,26 @@ public class Ex_6_Detail_SelectionSort {
         for (int i = 0; i < n - 1; i++) {
             // 아직 정렬되지 않은 부분의 가장 작은 요소의 인덱스를 기록한다.
             int min = i;
-            for (int j = i + 1; j < n; j++) {
 
-                /*
-                for (int m = 0; m < n; m++)
-                    System.out.printf("%3s\n", (m == i) ? '*' : (a[m] < a[min]) ? '+' : ' ');
-                */
-                if (a[j] < a[min]) {
-                    /*
-                    for (int k = 0; k < n; k++)
-                        System.out.printf("%3d", a[k]);
-                    System.out.println();
-                    */
+            for (int j = i + 1; j < n; j++)
+                if (a[j] < a[min])
                     min = j;
-                }
+
+            for (int m = 0; m < n; m++)
+                System.out.printf("%3s", (m == i) ? '*' : (m == min) ? '+' : ' ');
+            System.out.println();
+
+            for (int k = 0; k < n; k++)
+                System.out.printf("%3d", a[k]);
+            System.out.println();
             // 아직 정렬되지 않은 부분의 첫 요소와 가장 작은 요소를 교환한다.
             swap(a, i, min);
-            }
         }
+
+        System.out.println();
+        for (int k = 0; k < n; k++)
+            System.out.printf("%3d", a[k]);
+        System.out.println();
     }
 
     static void showResult(int[] x, int n) {
@@ -49,13 +51,11 @@ public class Ex_6_Detail_SelectionSort {
 
         for (int i = 0; i < n; i++) {
             System.out.print("x[" + i + "] : ");
-            int idx = stdIn.nextInt();
-            x[i] = idx;
+            x[i] = stdIn.nextInt();
         }
 
         System.out.println("선택 정렬 과정 출력 : ");
         detail_SelecSort(x, n);
-
         showResult(x, n);
     }
 }
--- src/chap06/SelectionSort.java
+++ src/chap06/SelectionSort.java
@@ -1,20 +1,47 @@
 package chap06;
 
+import java.util.Scanner;
+
 public class SelectionSort {
 
-    static void swap(int[]a , int idx1, int idx2) {
+    static void swap(int[] a, int idx1, int idx2) {
         int t = a[idx1];
         a[idx1] = a[idx2];
         a[idx2] = t;
     }
 
-		static void selectionSort(int[]a , int n) {     
-        for(int i = 0 ; i < n-1 ; i++) {
+    static void selectionSort(int[] a, int n) {
+        for (int i = 0; i < n - 1; i++) {
             int min = i;
-            for(int j = i+1 ; j < n ; j++) 
-                if(a[j] < a[min])
-                min = j;
-            swap(a, i, min);       
-       }
+
+            for (int k = 0; k < n; k++)
+                System.out.printf("%3d", a[k]);
+            System.out.println();
+
+            for (int j = i + 1; j < n; j++)
+                if (a[j] < a[min])
+                    min = j;
+            swap(a, i, min);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        System.out.print("배열 요소 갯수를 입력하세요. :");
+        int nx = stdIn.nextInt();
+        int[] x = new int[nx];
+
+        System.out.println("배열 요소를 입력하세요.");
+        for (int i = 0; i < nx; i++) {
+            System.out.print("a[" + i + "] : ");
+            x[i] = stdIn.nextInt();
+        }
+
+        selectionSort(x, nx);
+
+        System.out.println("선택 정렬 결과 : ");
+
+        for (int i = 0; i < nx; i++)
+            System.out.println("x[" + i + "] : " + x[i]);
     }
 }
