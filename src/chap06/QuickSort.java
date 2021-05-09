62f7ab91f0f10b833d23863eb6d64c3d7251027b
MinjunShin <pj0014@naver.com>
2021-05-09 18:16:37 +0900

    6.6 퀵 정렬 완성

--- src/chap06/QuickSort.java
+++ src/chap06/QuickSort.java
@@ -2,25 +2,33 @@ package chap06;
 
 import java.util.Scanner;
 
-public class Quick_Partiton {
+public class QuickSort {
+
+    static int count = 0;
+
     static void swap(int[] a, int idx1, int idx2) {
         int t = a[idx1];
         a[idx1] = a[idx2];
         a[idx2] = t;
+        count++;
     }
 
-    static void Partition(int[]a , int n) {
-        int pl = 0;
-        int pr = n-1;
-        int x = a[n / 2];
-
-        do {
-            while (a[pl] < x) pl++;
-            while (a[pr] > x) pr--;
-            if(pl <= pr)
-                swap(a, pl++, pr--);
-        } while(pl <= pr);
+    static void quickSort(int[] a, int left, int right) {
+        int pl = left;
+        int pr = right;
+        int x = a[(pl + pr) / 2];
         
+        do {
+            while (a[pl] < x)
+                pl++;
+            while (a[pr] > x)
+                pr--;
+            if (pl <= pr)
+                swap(a, pl++, pr--);
+        } while (pl <= pr);
+
+        if(left < pr) quickSort(a, left, pr);
+        if(pl < right) quickSort(a, pl, right); 
     }
 
     public static void main(String[] args) {
@@ -35,11 +43,13 @@ public class Quick_Partiton {
             x[i] = stdIn.nextInt();
         }
 
-        Partition(x, nx);
+        quickSort(x, 0, nx-1);
 
-        System.out.println("오름차 순으로 정렬한 결과 : ");
+        System.out.println("퀵 정렬한 결과 : ");
 
         for (int i = 0; i < nx; i++)
             System.out.println("x[" + i + "] : " + x[i]);
+
+        System.out.println(count + "번 실행하였다.");
     }
 }
